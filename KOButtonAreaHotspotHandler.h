75e3a4cda7e7384d5e5f5274ef144bc68ea72d2e
KOed <talentljl@gmail.com>
2009-01-27 02:55:49 +0000

    No need for KOTrackingRectData
    git-svn-id: https://welly.googlecode.com/svn/trunk@436 ace27a1a-5e51-0410-a197-e9aa1ca61478

--- KOButtonAreaHotspotHandler.h
+++ KOButtonAreaHotspotHandler.h
@@ -8,7 +8,20 @@
 
 #import <Cocoa/Cocoa.h>
 #import "KOMouseHotspotHandler.h"
-#import "KOTrackingRectData.h"
+
+#define fbComposePost @"\020"
+#define fbDeletePost @"dy\n"
+#define fbShowNote @"\t"
+#define fbShowHelp @"h"
+#define fbNormalToDigest @"\07""1\n"
+#define fbDigestToThread @"\07""2\n"
+#define fbThreadToMark @"\07""3\n"
+#define fbMarkToOrigin @"\07""4\n"
+#define fbOriginToNormal @"e"
+
+typedef enum {
+	COMPOSE_POST, DELETE_POST, SHOW_NOTE, SHOW_HELP, NORMAL_TO_DIGEST, DIGEST_TO_THREAD, THREAD_TO_MARK, MARK_TO_ORIGIN, ORIGIN_TO_NORMAL
+} KOButtonType;
 
 @class YLView;
 @interface KOButtonAreaHotspotHandler : KOMouseHotspotHandler <KOMouseHotspotDelegate> {
