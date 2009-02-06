51566aee7df94d313c5b911bca1a0000f4e230e0
gtCarrera <lvli007@gmail.com>
2009-02-06 03:48:23 +0000

    typo...
    git-svn-id: https://welly.googlecode.com/svn/trunk@452 ace27a1a-5e51-0410-a197-e9aa1ca61478

--- YLController.m
+++ YLController.m
@@ -1447,11 +1447,11 @@ static NSColor* colorUsingNearestAnsiColor(NSColor *rawColor, BOOL isBackground)
 #pragma mark -
 #pragma mark For restore settings
 - (IBAction)restoreSettings:(id)sender {
-	NSAlert *alert = [NSAlert alertWithMessageText:NSLocalizedString(@"Are you sure you want to restore all you font settings?", @"Sheet Title")
+	NSAlert *alert = [NSAlert alertWithMessageText:NSLocalizedString(@"Are you sure you want to restore all your font settings?", @"Sheet Title")
 									 defaultButton:NSLocalizedString(@"Confirm", @"Default Button")
 								   alternateButton:NSLocalizedString(@"Cancel", @"Cancel Button")
 									   otherButton:nil
-						 informativeTextWithFormat:NSLocalizedString(@"If you proceed, you will lost all you current font settings for Welly, and this operation is only encouraged when your font settings are missing. Are you sure you want to continue?", @"Sheet Message")];
+						 informativeTextWithFormat:NSLocalizedString(@"If you proceed, you will lost all your current font settings for Welly, and this operation is only encouraged when your font settings are missing. Are you sure you want to continue?", @"Sheet Message")];
 	if ([alert runModal] != NSAlertDefaultReturn)
 		return;
 	if([_telnetView isInPortalState]) {
