1bb271f91d4a1be62ca5e3bf6beeacc2502e51fd
boost <xi.wang@gmail.com>
2009-03-27 17:40:44 +0000

    remove c+p code
    git-svn-id: https://welly.googlecode.com/svn/trunk@547 ace27a1a-5e51-0410-a197-e9aa1ca61478

--- TYProxy.m
+++ TYProxy.m
@@ -13,7 +13,7 @@
 @implementation TYProxy
 
 // source code adapted from http://developer.apple.com/qa/qa2001/qa1234.html
-Boolean GetHTTPProxySetting(char *host, size_t hostSize, UInt16 *port)
+Boolean GetProxySetting(const char *protocol, char *host, size_t hostSize, UInt16 *port)
 // Returns the current HTTP proxy settings as a C string 
 // (in the buffer specified by host and hostSize) and 
 // a port number.
@@ -37,8 +37,9 @@ Boolean GetHTTPProxySetting(char *host, size_t hostSize, UInt16 *port)
     // Get the enable flag.  This isn't a CFBoolean, but a CFNumber.
     
     if (result) {
+        // kSCPropNetProxiesHTTPEnable
         enableNum = (CFNumberRef) CFDictionaryGetValue(proxyDict,
-                                                       kSCPropNetProxiesHTTPEnable);
+                                                       [NSString stringWithFormat:@"%sEnable", protocol]);
         
         result = (enableNum != NULL)
         && (CFGetTypeID(enableNum) == CFNumberGetTypeID());
@@ -54,8 +55,9 @@ Boolean GetHTTPProxySetting(char *host, size_t hostSize, UInt16 *port)
     // function will fail and this function will return false.
     
     if (result) {
+        // kSCPropNetProxiesHTTPProxy
         hostStr = (CFStringRef) CFDictionaryGetValue(proxyDict,
-                                                     kSCPropNetProxiesHTTPProxy);
+                                                     [NSString stringWithFormat:@"%sProxy", protocol]);
         
         result = (hostStr != NULL)
         && (CFGetTypeID(hostStr) == CFStringGetTypeID());
@@ -68,168 +70,9 @@ Boolean GetHTTPProxySetting(char *host, size_t hostSize, UInt16 *port)
     // Get the proxy port.
     
     if (result) {
+        // kSCPropNetProxiesHTTPPort
         portNum = (CFNumberRef) CFDictionaryGetValue(proxyDict,
-                                                     kSCPropNetProxiesHTTPPort);
-        
-        result = (portNum != NULL)
-        && (CFGetTypeID(portNum) == CFNumberGetTypeID());
-    }
-    if (result) {
-        result = CFNumberGetValue(portNum, kCFNumberIntType, &portInt);
-    }
-    if (result) {
-        *port = (UInt16) portInt;
-    }
-    
-    // Clean up.
-    
-    if (proxyDict != NULL) {
-        CFRelease(proxyDict);
-    }
-    if ( ! result ) {
-        *host = 0;
-        *port = 0;
-    }
-    return result;
-}
-
-Boolean GetHTTPSProxySetting(char *host, size_t hostSize, UInt16 *port)
-// Returns the current HTTPS proxy settings as a C string 
-// (in the buffer specified by host and hostSize) and 
-// a port number.
-{
-    Boolean             result;
-    CFDictionaryRef     proxyDict;
-    CFNumberRef         enableNum;
-    int                 enable;
-    CFStringRef         hostStr;
-    CFNumberRef         portNum;
-    int                 portInt;
-    
-    assert(host != NULL);
-    assert(port != NULL);
-    
-    // Get the dictionary.
-    
-    proxyDict = SCDynamicStoreCopyProxies(NULL);
-    result = (proxyDict != NULL);
-    
-    // Get the enable flag.  This isn't a CFBoolean, but a CFNumber.
-    
-    if (result) {
-        enableNum = (CFNumberRef) CFDictionaryGetValue(proxyDict,
-                                                       kSCPropNetProxiesHTTPSEnable);
-        
-        result = (enableNum != NULL)
-        && (CFGetTypeID(enableNum) == CFNumberGetTypeID());
-    }
-    if (result) {
-        result = CFNumberGetValue(enableNum, kCFNumberIntType,
-                                  &enable) && (enable != 0);
-    }
-    
-    // Get the proxy host.  DNS names must be in ASCII.  If you 
-    // put a non-ASCII character  in the "Secure Web Proxy"
-    // field in the Network preferences panel, the CFStringGetCString
-    // function will fail and this function will return false.
-    
-    if (result) {
-        hostStr = (CFStringRef) CFDictionaryGetValue(proxyDict,
-                                                     kSCPropNetProxiesHTTPSProxy);
-        
-        result = (hostStr != NULL)
-        && (CFGetTypeID(hostStr) == CFStringGetTypeID());
-    }
-    if (result) {
-        result = CFStringGetCString(hostStr, host,
-                                    (CFIndex) hostSize, kCFStringEncodingASCII);
-    }
-    
-    // Get the proxy port.
-    
-    if (result) {
-        portNum = (CFNumberRef) CFDictionaryGetValue(proxyDict,
-                                                     kSCPropNetProxiesHTTPSPort);
-        
-        result = (portNum != NULL)
-        && (CFGetTypeID(portNum) == CFNumberGetTypeID());
-    }
-    if (result) {
-        result = CFNumberGetValue(portNum, kCFNumberIntType, &portInt);
-    }
-    if (result) {
-        *port = (UInt16) portInt;
-    }
-    
-    // Clean up.
-    
-    if (proxyDict != NULL) {
-        CFRelease(proxyDict);
-    }
-    if ( ! result ) {
-        *host = 0;
-        *port = 0;
-    }
-    return result;
-}
-
-Boolean GetSOCKSProxySetting(char *host, size_t hostSize, UInt16 *port)
-// Returns the current SOCKS proxy settings as a C string 
-// (in the buffer specified by host and hostSize) and 
-// a port number.
-{
-    Boolean             result;
-    CFDictionaryRef     proxyDict;
-    CFNumberRef         enableNum;
-    int                 enable;
-    CFStringRef         hostStr;
-    CFNumberRef         portNum;
-    int                 portInt;
-    
-    assert(host != NULL);
-    assert(port != NULL);
-    
-    // Get the dictionary.
-    
-    proxyDict = SCDynamicStoreCopyProxies(NULL);
-    result = (proxyDict != NULL);
-    
-    // Get the enable flag.  This isn't a CFBoolean, but a CFNumber.
-    
-    if (result) {
-        enableNum = (CFNumberRef) CFDictionaryGetValue(proxyDict,
-                                                       kSCPropNetProxiesSOCKSEnable);
-        
-        result = (enableNum != NULL)
-        && (CFGetTypeID(enableNum) == CFNumberGetTypeID());
-    }
-    if (result) {
-        result = CFNumberGetValue(enableNum, kCFNumberIntType,
-                                  &enable) && (enable != 0);
-    }
-    
-    // Get the proxy host.  DNS names must be in ASCII.  If you 
-    // put a non-ASCII character  in the "Secure Web Proxy"
-    // field in the Network preferences panel, the CFStringGetCString
-    // function will fail and this function will return false.
-    
-    if (result) {
-        hostStr = (CFStringRef) CFDictionaryGetValue(proxyDict,
-                                                     kSCPropNetProxiesSOCKSProxy);
-        
-        result = (hostStr != NULL)
-        && (CFGetTypeID(hostStr) == CFStringGetTypeID());
-    }
-    if (result) {
-        result = CFStringGetCString(hostStr, host,
-                                    (CFIndex) hostSize, kCFStringEncodingASCII);
-    }
-    
-    // Get the proxy port.
-    
-    if (result) {
-        portNum = (CFNumberRef) CFDictionaryGetValue(proxyDict,
-                                                     kSCPropNetProxiesSOCKSPort);
+                                                     [NSString stringWithFormat:@"%sPort", protocol]);
         
         result = (portNum != NULL)
         && (CFGetTypeID(portNum) == CFNumberGetTypeID());
@@ -257,17 +100,17 @@ Boolean GetSOCKSProxySetting(char *host, size_t hostSize, UInt16 *port)
     const size_t hostSize = 64;
     char host[hostSize];
     UInt16 port;
-    GetSOCKSProxySetting(host, hostSize, &port);
+    GetProxySetting("HTTP", host, hostSize, &port);
     if (*host) {
         if (port == 0) port = 1080;
         return [NSString stringWithFormat:@"ProxyCommand=/usr/bin/nc -x %s:%hu %%h %%p", host, port];
     }
-    GetHTTPProxySetting(host, hostSize, &port);
+    GetProxySetting("SOCKS", host, hostSize, &port);
     if (*host) {
         if (port == 0) port = 80;
         return [NSString stringWithFormat:@"ProxyCommand=/usr/bin/nc -X connect -x %s:%hu %%h %%p", host, port];
     }
-    GetHTTPSProxySetting(host, hostSize, &port);
+    GetProxySetting("HTTPS", host, hostSize, &port);
     if (*host) {
         if (port == 0) port = 443;
         return [NSString stringWithFormat:@"ProxyCommand=/usr/bin/nc -X connect -x %s:%hu %%h %%p", host, port];
