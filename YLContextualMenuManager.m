30feea70e1ccbfded7fb5a3bb65391494d797ccf
KOed <talentljl@gmail.com>
2010-04-16 11:51:22 +0000

    User customized contextual menu support. Remove hard code with shortURL and google. These have been put in localized contextualMenuItem.plist
    git-svn-id: https://welly.googlecode.com/svn/trunk@767 ace27a1a-5e51-0410-a197-e9aa1ca61478

--- YLContextualMenuManager.m
+++ YLContextualMenuManager.m
@@ -8,8 +8,6 @@
 //  new interface, by boost @ 9#
 
 #import "YLContextualMenuManager.h"
-#import "YLView.h"
-#import "WLTerminal.h"
 #import "YLController.h"
 #import "WLEmoticonsPanelController.h"
 #import "SynthesizeSingleton.h"
@@ -17,11 +15,7 @@
 
 NSString *const WLContextualMenuItemTitleFormatAttributeName = @"Title Format";
 NSString *const WLContextualMenuItemURLFormatAttributeName = @"URL Format";
-NSString *const WLOpenURLMenuItemFilename = @"OpenURLMenuItems";
-
-@interface YLContextualMenuManager ()
-+ (NSString *)extractShortURL:(NSString *)s;
-@end
+NSString *const WLOpenURLMenuItemFilename = @"contextualMenuItems";
 
 @implementation YLContextualMenuManager
 SYNTHESIZE_SINGLETON_FOR_CLASS(YLContextualMenuManager);
@@ -35,35 +29,31 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(YLContextualMenuManager);
 			// but there is only one shared instance.
 			// So we need to make sure this array have been initialized only once
 			if (!_openURLItemArray) {
+				NSBundle *mainBundle = [NSBundle mainBundle];
+				NSString *preferredLocalizationName = (NSString *)[[mainBundle preferredLocalizations] objectAtIndex:0];
 				_openURLItemArray = [[NSArray arrayWithContentsOfFile:
-									  [[NSBundle mainBundle] pathForResource:WLOpenURLMenuItemFilename 
-																	  ofType:@"plist"]] copy];
+									  [mainBundle pathForResource:WLOpenURLMenuItemFilename 
+														   ofType:@"plist" 
+													  inDirectory:nil 
+												  forLocalization:preferredLocalizationName]] copy];
 			}
 		}
     }
     return self;
 }
 
-+ (NSString *)extractShortURL:(NSString *)s {
-    NSMutableString *result = [NSMutableString string];
-    for (int i = 0; i < [s length]; i++) {
-        unichar c = [s characterAtIndex: i];
-        if (('0' <= c && c <= '9') ||
-            ('a' <= c && c <= 'z') ||
-            ('A' <= c && c <= 'Z'))
-            [result appendString:[NSString stringWithCharacters:&c length:1]];
-    }
-    return result;
-}
-
 + (NSMenuItem *)menuItemWithDictionary:(NSDictionary *)dictionary 
-							   selectedString:(NSString *)s {
-	NSString *title = [NSString stringWithFormat:[dictionary valueForKey:WLContextualMenuItemTitleFormatAttributeName], s, nil];
-	NSString *url = [NSString stringWithFormat:[dictionary valueForKey:WLContextualMenuItemURLFormatAttributeName], s, nil];
+						selectedString:(NSString *)s {
+	NSString *title = [NSString stringWithFormat:
+					   [dictionary valueForKey:WLContextualMenuItemTitleFormatAttributeName],
+					   s, nil];
+	NSString *url = [NSString stringWithFormat:
+					 [dictionary valueForKey:WLContextualMenuItemURLFormatAttributeName], 
+					 [s stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding], nil];
 	NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:title 
-												   action:@selector(openURL:) 
-											keyEquivalent:@""];
-	//[item autorelease];
+												  action:@selector(openURL:) 
+										   keyEquivalent:@""];
+	[item setToolTip:url];
 	[item setRepresentedObject:url];
 	return item;
 }
@@ -71,10 +61,6 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(YLContextualMenuManager);
 + (NSMenu *)menuWithSelectedString:(NSString*)selectedString {
     NSMenu *menu = [[[NSMenu alloc] init] autorelease];
 
-	/* comment: why not just using the url recognition? */
-    YLView *view = [[YLController sharedInstance] telnetView];
-	
-	NSString *shortURL = [self extractShortURL:selectedString];
 	// Remove all '\n' '\r' ' ' from the URL string
 	NSString *longURL = [selectedString stringByReplacingOccurrencesOfString:@"\n" withString:@""];
 	longURL = [longURL stringByReplacingOccurrencesOfString:@"　" withString:@""];
@@ -87,31 +73,12 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(YLContextualMenuManager);
 						action:@selector(openURL:)
 				 keyEquivalent:@""];
 	}
-	
-	if ([[view frontMostTerminal] bbsType] == WLMaple) {
-		// Firebird BBS seldom use these	
-		if ([shortURL length] > 0 && [shortURL length] < 8) {
-			[menu addItemWithTitle:[@"0rz.tw/" stringByAppendingString:shortURL]
-							action:@selector(openURL:)
-					 keyEquivalent:@""];
-			
-			[menu addItemWithTitle:[@"tinyurl.com/" stringByAppendingString:shortURL]
-							action:@selector(openURL:)
-					 keyEquivalent:@""];
-		}
-	}
-/* */
+
     if ([selectedString length] > 0) {
     
         [menu addItemWithTitle:NSLocalizedString(@"Search in Spotlight", @"Menu")
                         action:@selector(spotlight:)
                  keyEquivalent:@""];
-
-        [menu addItemWithTitle:NSLocalizedString(@"Search in Google", @"Menu")
-                        action:@selector(google:)
-                 keyEquivalent:@""];
-        
-        [menu addItem:[NSMenuItem separatorItem]];
         
         [menu addItemWithTitle:NSLocalizedString(@"Look Up in Dictionary", @"Menu")
                         action:@selector(lookupDictionary:)
@@ -139,14 +106,7 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(YLContextualMenuManager);
 					[item release];
 				}
 			}
-		} /* else {
-			NSArray *defaultMenu = [NSArray arrayWithObject:
-									[NSDictionary dictionaryWithObjectsAndKeys:
-									 @"testEntry", WLContextualMenuItemTitleFormatAttributeName,
-									 @"%@", WLContextualMenuItemURLFormatAttributeName, nil]];
-			NSString *path = [[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:WLOpenURLMenuItemFilename] stringByAppendingPathExtension:@"plist"];
-			[defaultMenu writeToFile:path atomically:YES];
-		} */
+		}
     }
 
     for (int i = 0; i < [menu numberOfItems]; ++i) {
@@ -182,18 +142,11 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(YLContextualMenuManager);
 		u = [@"http://" stringByAppendingString:[u stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
 	}
     [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:u]];
-	NSLog(@"opening url:%@", u);
 }
 
 + (IBAction)spotlight:(id)sender {
     NSString *u = [sender representedObject];
     HISearchWindowShow((CFStringRef)u, kNilOptions);
-}
-
-+ (IBAction)google:(id)sender {
-    NSString *u = [sender representedObject];
-    u = [@"http://www.google.com/search?q=" stringByAppendingString:[u stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:u]];
 }
 
 + (IBAction)lookupDictionary:(id)sender {
