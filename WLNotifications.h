24937a27e158a367346a571868bf446e0758f37d
KOed <talentljl@gmail.com>
2010-05-01 14:58:08 +0000

    oops, forget a file...
    git-svn-id: https://welly.googlecode.com/svn/trunk@783 ace27a1a-5e51-0410-a197-e9aa1ca61478

---
+++ WLNotifications.h
@@ -0,0 +1,11 @@
+/*
+ *  WLNotifications.h
+ *  Welly
+ *
+ *  Created by K.O.ed on 10-5-1.
+ *  Copyright 2010 Welly Group. All rights reserved.
+ *
+ */
+
+#define WLNotificationSiteDidChangeShouldEnableMouse @"WLNotificationSiteDidChangeShouldEnableMouse"
+#define WLNotificationSiteDidChangeEncoding @"WLNotificationSiteDidChangeEncoding"
