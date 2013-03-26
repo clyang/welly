31647924f5323c104210a21761e7f66e8c8d4f5a
KOed <talentljl@gmail.com>
2013-03-26 23:07:21 +0800

    Deal the case when ratioH < ratioW
    Besides, change the setFont: method. Now the original size parameters
    are stored when entering fullscreen/presentation, and restored when
    exiting. This should prevent ruining shape by repeating enter/exit
    fullscreen.

--- WLPresentationController.h
+++ WLPresentationController.h
@@ -9,21 +9,7 @@
 #import <Cocoa/Cocoa.h>
 @protocol WLPresentationModeProcessor;
 
-@interface WLPresentationController : NSObject {
-	// The views necessary for full screen and reset
-	NSView *_targetView;
-	NSView *_superView;
-	
-	// NSWindows needed...
-	NSWindow *_fullScreenWindow;
-	NSWindow *_originalWindow;
-	
-	NSRect _originalFrame;
-	
-	// State variable
-	BOOL _isInPresentationMode;
-	CGFloat _screenRatio;
-}
+@interface WLPresentationController : NSObject
 @property (readonly) BOOL isInPresentationMode;
 
 // Init functions
@@ -37,9 +23,5 @@
 // Handle functions
 - (void)togglePresentationMode;
 - (void)exitPresentationMode;
-
-// Preprocess functions for TerminalView
-- (void)processBeforeEnter;
-- (void)processBeforeExit;
 
 @end
