760521e9fe35937e6c48c7deb46ee883704bbad3
clyang <clyang@clyang.net>
2017-09-23 15:26:30 +0800

    telnet will be removed from MacOS High Sierra by Apple. Hence, an execuatable telnet is bundled and will be used as the default telnet client.

--- proxy.sh
+++ proxy.sh
@@ -3,4 +3,4 @@ echo "Connecting to BBS via Websockets..."
 BASEDIR=$(dirname "$0")
 $BASEDIR/usock2wsock -u $1 -r $2 &
 sleep 2
-/usr/bin/telnet -8 -u /tmp/telnetBYwebsocket.$2.sock
+$BASEDIR/telnet -8 -u /tmp/telnetBYwebsocket.$2.sock
