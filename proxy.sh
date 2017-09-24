8cc836895937a8e326a72bc11cc8771fb5c92b4e
clyang <clyang@clyang.net>
2017-09-25 00:14:51 +0800

    Fix issue when Welly is running in the directory with space in name Also remove old Sparkle framework so we can move to the latest version

--- proxy.sh
+++ proxy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Connecting to BBS via Websockets..."
 BASEDIR=$(dirname "$0")
-$BASEDIR/usock2wsock -u $1 -r $2 &
+"$BASEDIR/usock2wsock" -u $1 -r $2 &
 sleep 2
-$BASEDIR/telnet -8 -u /tmp/telnetBYwebsocket.$2.sock
+"$BASEDIR/telnet" -8 -u /tmp/telnetBYwebsocket.$2.sock
